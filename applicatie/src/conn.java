9b9feeaf4a87bc08c047db32e7c064e557fda86d
SM-MIGOS <102881134+SM-MIGOS@users.noreply.github.com>
2022-05-11 20:00:20 -0300

    MIGOS
    conn file

---
+++ applicatie/src/conn.java
@@ -0,0 +1,28 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
+
+public class conn
+{
+    public static void main (String[] args)
+    {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/memory_game", "root", "1234");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from users");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("user_usernaam"));
+            }
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();;
+        }
+    }
+}
